6ae4b273a64870be7c6dfbd14abbf9048c8d8092
HeungGiChoi <pamo23@naver.com>
2026-02-24 20:00:11 +0900

    260224 modal update

--- JS/project_modal.js
+++ JS/project_modal.js
@@ -1,11 +1,18 @@
-const modal = document.getElementById("modalContainer");
-const close_Btn = document.querySelector(".close");
+const modal = document.getElementById("modalContainer"); // modal 컨테이너
+const close_Btn = document.querySelector(".close"); //  modal 닫기 버튼
 
-const open_Btn = document.querySelectorAll(".modal-button");
+const open_Btn = document.querySelectorAll(".modal-button"); // modal 열기 버튼
 
-const modalTitle = modal.querySelector("h3");
-const modalSkills = modal.querySelector(".skills");
-const modalDesc = modal.querySelector(".description");
+const modalTitle = modal.querySelector("h3"); // 제목
+const modalSummary = modal.querySelector(".summary"); // 요약
+const modalSkills = modal.querySelector(".skills"); // 기술스택 head
+const modalSkillList = modal.querySelector(".skill-list"); // 기술스택 리스트
+const modalMembers = modal.querySelector(".members"); // 팀원
+const modalSchedule = modal.querySelector(".modal-schedule"); // 프로젝트 일정
+const modalProduct = modal.querySelector(".product"); // 배포 결과물 또는 이미지
+const modalBackground = modal.querySelector(".background"); // 프로젝트 배경
+const modalDesc = modal.querySelector(".description"); // 상세 내용
+const modalMeaning = modal.querySelector(".meaning"); // 배운점
 
 open_Btn.forEach((button, index) => {
   button.addEventListener("click", () => {
@@ -15,17 +22,43 @@ open_Btn.forEach((button, index) => {
         const project_data = data[index];
 
         modalTitle.textContent = project_data.title;
+        modalSummary.innerHTML = `
+          <i class="fa-solid fa-pen"></i><strong> Summary </strong> <br><br>&emsp;${project_data.summary}
+          `;
         modalSkills.innerHTML = `
-          <strong>Technology Stack</strong> <br><br>
+          <i class="fa-solid fa-screwdriver-wrench"></i><strong> Technology Stack</strong> <br><br>
+          `;
+        modalSkillList.innerHTML = `
+        <ul>
+          ${project_data.skills.map((item) => `<li data-name=${item.name}>${item.img}</li>`).join("")}
+        </ul>
+        `;
+        modalMembers.innerHTML = `
+          <i class="fa-solid fa-people-group"></i><strong> 참여인원 : </strong> ${project_data.members}
+        `;
+        modalSchedule.innerHTML = `
+          <i class="fa-solid fa-calendar"></i><strong> 기간 : </strong> ${project_data.schedule}
+        `;
+        modalProduct.innerHTML = `
+          <i class="fa-solid fa-link"></i><strong> Deployment : </strong> ${project_data.product}
+        `;
+        modalBackground.innerHTML = `
+          &#129300;<strong> Background </strong> <br><br>
           <ul>
-            ${project_data.skills.map((item) => `<li>${item}</li>`).join("")}
-          <ul>
+            ${project_data.background.map((item) => `<li>${item}</li>`).join("")}
+          </ul>
         `;
         modalDesc.innerHTML = `
-          <strong>Summary: </strong> ${project_data.description}<br><br>
+          <i class="fa-solid fa-clipboard"></i><strong> Description </strong> <br><br>
           <ul>
-            ${project_data.details.map((item) => `<li>${item}</li>`).join("")}
+            ${project_data.description.map((item) => `<li>${item}</li>`).join("")}
+          </ul>
+        `;
+        modalMeaning.innerHTML = `
+          <i class="fa-solid fa-magnifying-glass"></i><strong> Meaning </strong> <br><br>
           <ul>
+            ${project_data.meaning.map((item) => `<li>${item}</li>`).join("")}
+          </ul>
         `;
         modal.classList.add("show");
         document.body.style.overflow = "hidden";
